79af918b121085a31de0c83419a46cb05f819e06
kaylaaditya <kaylaaditya123@outlook.com>
2024-03-01 20:00:30 +0700

    selesai mengerjakan landing page

--- app/Models/Buku.php
+++ app/Models/Buku.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Buku extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    
 
     public $timestamps = false;
     // nama tabel jika tidak jamak (tanpa s/es)
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perpustakaan Digital</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="/images/css/app.css" />
 
-    
+
 </head>
+
 <body class="bg-gray-100">
-    
-    <div class="container mx-auto p-8">
-        <h1 class="text-4xl font-bold mb-8">Selamat datang di Perpustakaan Digital</h1>
-        
-        <p class="text-lg mb-4">Selamat Datang di Perpustakaan Digital Kami!
-        Dapatkan akses ke koleksi terbaru dari berbagai genre, penulis terkenal, dan topik menarik.</p>
 
-        <a href="{{ route('layouts.tabel-data') }}" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Jelajahi Koleksi</a>
-    </div>
-
+    <header>
+        <div class="container-fluid">
+            <div id="welcome-section" class="intro-text">
+                <div class="logo">
+                    <img src="http://localhost:8000/logoperpus.png" alt="Logo" class="light-background" style="width: 100px;">
+                </div>
+                <div class="intro-lead-in">Selamat Datang Di Perpustakaan Digital</div>
+                <h2>Selamat Datang di Perpustakaan Digital Kami!
+                    apatkan akses ke koleksi terbaru dari berbagai genre, penulis terkenal, dan topik menarik.
+                </h2>
+                <a href="{{ route('login.login') }}" class="page-scroll btn btn-xl">Jelajahi Aplikasi</a>
+            </div>
+        </div>
+    </header>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
