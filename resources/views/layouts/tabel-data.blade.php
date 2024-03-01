862cc8ed1d6f06e273975aeacc95f44a903645e2
kaylaaditya <kaylaaditya123@outlook.com>
2024-03-01 17:30:09 +0700

    perbaikan softdeletes

--- resources/views/layouts/tabel-data.blade.php
+++ resources/views/layouts/tabel-data.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    c
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Dashboard|PERPUSWEB</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
